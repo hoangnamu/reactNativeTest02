9815604789c142edb9e5f0a83ac510ba9dd27b9c
namlehoang <hoangnamle1998@gmail.com>
2020-04-15 22:15:22 +0700

    user test done

--- src/user/__test__/User.component.test.js
+++ src/user/__test__/User.component.test.js
@@ -5,15 +5,22 @@ import User from './../User.component';
 
 describe('Homepage Test', () => {
     it('should render snapshot test for user', () => {
-        
+        let props = {navigation: {state: {params: {userType: 'professional'}}}}
+        const snap = shallow(<User {...props}/>)
+        expect(snap).toMatchSnapshot()
     })
 
     it('should simulate & render avatar if avatar is exist ', () => {
-
+        let props = {navigation: {state: {params: {userType: 'professional'}}}}
+        const snap = shallow(<User {...props}/>)
+        expect(snap.find('Image').exists()).toBe(true)
+        // expect(snap.find('Image')).to.have.lengthOf(1)
     })
 
     it('should render empty when avatar is empty ', () => {
-
+        let props = {navigation:{state:{params:{userType:'anonymous'}}}}
+        const snap = shallow(<User{...props}/>)
+        expect(snap.find('Image').exists()).toBe(false)
     })
 
 })
