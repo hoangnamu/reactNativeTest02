64cfbdefe6e5005409a5119c9a77b00d7b46f3db
namlehoang <hoangnamle1998@gmail.com>
2020-04-16 10:23:08 +0700

    should render snapshot test for homepage should show state as message expect when mount component

--- src/homepage/__test__/Homepage.component.test.js
+++ src/homepage/__test__/Homepage.component.test.js
@@ -5,26 +5,33 @@ import Homepage from './../Homepage.component';
 
 describe('Homepage Test', () => {
     it('should render snapshot test for homepage', () => {
-
+        const snap = shallow(<Homepage/>)
+        expect(snap).toMatchSnapshot()
     })
 
     it('should show state as message expect when mount component', () => {
-
+        const expectMessage = 'Welcome to user board'
+        const wrapper = shallow(<Homepage/>)
+        expect(wrapper.state().title).toEqual(expectMessage)
     })
 
     it('should navigate to anonymous users', () => {
+        expect(false).toBe(true)
 
     })
 
     it('should turn trial version when navigating to anonymous user', () => {
+        expect(false).toBe(true)
 
     })
 
     it('should navigate to professional users', () => {
+        expect(false).toBe(true)
 
     })
 
     it('should navigate to premium users', () => {
-       
+        expect(false).toBe(true)
+
     })
 })
